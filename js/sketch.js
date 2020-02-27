3b80f600bc9ab6f8e15aa48d8868db3a77f7a42d
carlofabyss <carlofabyss@hotmail.com>
2020-02-26 18:55:00 -0800

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -1,7 +1,33 @@
+//travel to the stars on a wacky adventure with elon musk
+let random_cat_facts;
+
 function setup() {
+    //lets make it responsive
     createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
     background(0);
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+function keyPressed() {
+    requestcats();
+}
+
+function requestcats() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            random_cat_facts = this.responseText;
+            console.log(random_cat_facts);
+        }
+    };
+
+    xhttp.open("GET", "https://cat-fact.herokuapp.com/facts", true);
+    xhttp.send();
+
 }
