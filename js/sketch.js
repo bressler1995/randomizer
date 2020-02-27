dff3365ed5f0208f03375a12fda9384909255da5
carlofabyss <carlofabyss@hotmail.com>
2020-02-26 19:40:01 -0800

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -30,6 +30,7 @@ function requestcats() {
 
     if("withCredentials" in xhttp) {
         xhttp.open("GET", "https://catfact.ninja/facts/random?animal_type=cat&amount=2", true);
+        xhttp.setRequestHeader('Accept: application/json');
         xhttp.send();
     }
 
