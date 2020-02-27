2ccae71f14541ea88add9393b16a1111997f8d40
carlofabyss <carlofabyss@hotmail.com>
2020-02-26 19:56:11 -0800

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -1,5 +1,6 @@
 //travel to the stars on a wacky adventure with elon musk
 let initialresponse;
+let catfacts = [];
 
 function setup() {
     //lets make it responsive
@@ -23,7 +24,11 @@ function requestcats() {
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             initialresponse = JSON.parse(this.responseText);
-            console.log(initialresponse.data);
+            // console.log(initialresponse.data);
+            for (i = 0; i < initialresponse.data.length; i++) {
+                catfacts.push(initialresponse.data[i].fact);
+            }
+            console.log(catfacts);
         }
     };
 
