a6adc4ccb667b16d406b870945f9aa1875535ebd
carlofabyss <carlofabyss@hotmail.com>
2020-03-02 17:37:23 -0800

    Attempt at random

--- js/sketch.js
+++ js/sketch.js
@@ -2,7 +2,8 @@
 let initialresponse;
 let catfacts = [];
 let catpics = [];
-let catpics_url = ['img/spacecat.jpg', 'img/orangecat.jpg'];
+let catpics_url = ['img/spacecat.jpg', 'img/orangecat.jpg', 'img/wildcat.jpg', 'img/yawningcat.jpg', 'img/laptopcat.gif'];
+let catsrandom = [];
 let catsset = false;
 let appstate = 0;
 let button;
@@ -10,6 +11,9 @@ let button;
 function preload() {
     catpics[0] = loadImage(catpics_url[0]);
     catpics[1] = loadImage(catpics_url[1]);
+    catpics[2] = loadImage(catpics_url[2]);
+    catpics[3] = loadImage(catpics_url[3]);
+    catpics[4] = loadImage(catpics_url[4]);
 }
 
 function setup() {
@@ -25,8 +29,46 @@ function draw() {
     background('#380572');
     if(catsset) {
         if (appstate == 0) {
+            push();
             imageMode(CENTER);
             image(catpics[0], windowWidth / 2, windowHeight / 2, 400, 263);
+            pop();
+        } else if(appstate == 1) {
+            push();
+            imageMode(CENTER);
+            image(catpics[1], windowWidth / 2, windowHeight / 2, 400, 266);
+            fill(255);
+            textFont('Arial');
+            textSize(14);
+            text(catfacts[catsrandom[0]], 20, windowHeight - 50, windowWidth - 20, windowHeight -10);
+            pop();
+        } else if(appstate == 2) {
+            push();
+            imageMode(CENTER);
+            image(catpics[2], windowWidth / 2, windowHeight / 2, 400, 271);
+            fill(255);
+            textFont('Arial');
+            textSize(14);
+            text(catfacts[catsrandom[1]], 20, windowHeight - 50, windowWidth - 20, windowHeight -10);
+            pop();
+        } else if(appstate == 3) {
+            push();
+            imageMode(CENTER);
+            image(catpics[3], windowWidth / 2, windowHeight / 2, 400, 266);
+            fill(255);
+            textFont('Arial');
+            textSize(14);
+            text(catfacts[catsrandom[2]], 20, windowHeight - 50, windowWidth - 20, windowHeight -10);
+            pop();
+        } else if(appstate == 4) {
+            push();
+            imageMode(CENTER);
+            image(catpics[4], windowWidth / 2, windowHeight / 2, 400, 266);
+            fill(255);
+            textFont('Arial');
+            textSize(14);
+            text(catfacts[catsrandom[3]], 20, windowHeight - 50, windowWidth - 20, windowHeight -10);
+            pop();
         }
     }
     
@@ -44,10 +86,17 @@ function requestcats() {
             // console.log(initialresponse.data);
             for (i = 0; i < initialresponse.data.length; i++) {
                 catfacts.push(initialresponse.data[i].fact);
-                console.log(initialresponse.data[i].fact);
+                // console.log(initialresponse.data[i].fact);
+            }
+
+            for (i = 0; i < 4; i++) {
+                catsrandom[i] = round(random(0, catfacts.length - 1));
+                console.log("Number " + (i + 1) + ":" + catsrandom[i]);
             }
 
             catsset = true;
+
+            
         }
     };
 
@@ -64,5 +113,16 @@ function start_catting() {
 }
 
 function loopcats() {
+    setTimeout(function(){
 
+        if(appstate == 1) {
+            appstate++;
+            loopcats();
+        } else if(appstate == 2) {
+            appstate++;
+            loopcats();
+        } else if(appstate == 3) {
+            appstate++;
+        }
+    }, 500);
 }
